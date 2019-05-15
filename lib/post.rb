1c3d736a2f05bd3dfb422a61b4146cde75ff5b64
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-15 03:02:20 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,12 +1,6 @@
 require_relative "../lib/author.rb"
 class Post
   attr_accessor :title, :author
-  # attr_reader :author 
-  
-  # def author=(Author.new)
-  #   @author = Author.new
-  # end
-  
   def initialize
     @title
     @author
