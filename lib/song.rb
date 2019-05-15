2f7ea3a6724ff8343e6b8053eb2a33c4fd4375ee
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-15 02:56:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require_relative "../lib/artist.rb"
 class Song
   attr_accessor :title
   attr_reader :artist
-  def artist=(Artist)
-    @artist = Artist
+  def artist=(Artist.new)
+    @artist = Artist.new
   end
 end
